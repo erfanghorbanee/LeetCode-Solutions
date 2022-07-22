e2e9d8e75f53a93e597236bd48e86546d8794945
efi <erfanghorbanee@gmail.com>
2022-07-22 10:40:15 +0430

    problem 283 added

---
+++ Solutions/283.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        
+        p =0
+        
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                if p!= i:
+                    nums[i],nums[p] = nums[p],nums[i]
+                p +=1
+        
