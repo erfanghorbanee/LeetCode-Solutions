46d05c41f55d707c0dbac1ca640e234e786403cc
efi <erfanghorbanee@gmail.com>
2022-07-25 09:33:04 +0430

    Code style with black

--- Solutions/1464.py
+++ Solutions/1464.py
@@ -4,16 +4,16 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        
+
         max1, max2 = 0, 0
-        
+
         for num in nums:
 
             if num > max1:
                 max2 = max1
                 max1 = num
-            
+
             elif num > max2:
                 max2 = num
-                
-        return (max1-1)*(max2-1)
+
+        return (max1 - 1) * (max2 - 1)
