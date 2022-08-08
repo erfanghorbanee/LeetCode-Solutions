6ae4a5c7bbee7404455898e04731ff2b1c56c00d
efi <erfanghorbanee@gmail.com>
2022-08-08 11:22:47 +0430

    style by black

--- Solutions/53.py
+++ Solutions/53.py
@@ -4,15 +4,15 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        
+
         max_sub = nums[0]
         sum = 0
-        
+
         for n in nums:
             if sum < 0:
                 sum = 0
-            
-            sum+= n
+
+            sum += n
             max_sub = max(max_sub, sum)
-        
+
         return max_sub
